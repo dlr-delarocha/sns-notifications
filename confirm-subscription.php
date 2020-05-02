3109ecf169a8b3113cc19e1af2cc6f009c9fe1d7
Alberto De la Rocha <alberto.delarocha@uabc.edu.mx>
2020-05-02 15:15:24 -0700

    Update confirm-subscription.php

--- confirm-subscription.php
+++ confirm-subscription.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Aws\Sns\Message;
+use Aws\Sns\MessageValidator;
 
 /*
 |--------------------------------------------------------------------------
